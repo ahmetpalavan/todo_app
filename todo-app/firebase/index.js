98cd42fa1a77a75f3f7b27df4ecf2d9e9958dbb2
ahmetpalavan <ahmetpalavann@gmail.com>
2022-08-13 17:30:38 +0300

    Loading

---
+++ todo-app/firebase/index.js
@@ -0,0 +1,18 @@
+import {initializeApp} from 'firebase/app'
+import {getFirestore} from 'firebase/firestore'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDsGvA35iHIefhUDJJq3jWsLd08QssbNug",
+    authDomain: "modern-react-app-5ee64.firebaseapp.com",
+    projectId: "modern-react-app-5ee64",
+    storageBucket: "modern-react-app-5ee64.appspot.com",
+    messagingSenderId: "727371863945",
+    appId: "1:727371863945:web:580ce69b08a3f894ff7358",
+    measurementId: "G-8025V643SR"
+};
+
+initializeApp(firebaseConfig);
+const db=getFirestore()
+
+export {db}
